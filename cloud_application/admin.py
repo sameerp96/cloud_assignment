ec3c75e6e8579143b9f99ee7bc23cad3b2903edf
nkrish4x <naveen.garagakrishnamurthy2@mail.dcu.ie>
2019-11-16 19:25:03 +0000

    first

---
+++ cloud_application/admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from cloud_application.models import AirPollution
+
+# Register your models here.
+admin.site.register(AirPollution)
---
+++ cloud_application/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class AirPollution(models.Model):
+    country = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    place = models.CharField(max_length=200)
+    pollutants = models.CharField(max_length=200)
+    
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.country
